4da6f48e222298c4f32af5f4caa1cda6e19d9674
Nate Morton <nathan.j.morton@gmail.com>
2017-03-20 12:59:52 -0700

    adding Google map script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,12 @@
-function myMap()
-{
-  myCenter=new google.maps.LatLng(41.878114, -87.629798);
-  var mapOptions= {
-    center:myCenter,
-    zoom:12, scrollwheel: false, draggable: false,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
-
-  var marker = new google.maps.Marker({
-    position: myCenter,
-  });
-  marker.setMap(map);
+function initMap() {
+        var HaightAshbury = {lat: 37.770015, lng: -122.446937};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: HaightAshbury
+        });
+        var marker = new google.maps.Marker({
+          position: HaightAshbury,
+          map: map
+        });
 }
+
