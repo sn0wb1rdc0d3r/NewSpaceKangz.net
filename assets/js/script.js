3b8dc6b363e9402d6a533092042bc553abda4ca1
Nate Morton <nathan.j.morton@gmail.com>
2017-03-22 23:43:36 -0700

    nav opacity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,4 @@
-// w3.js
-function w3IncludeHTML(){
-  w3.includeHTML();
-}
-$(document).ready(w3IncludeHTML);
+
 
 // google map
 function initMap() {
@@ -19,19 +15,15 @@ function initMap() {
 $(document).ready(initMap);
 
 // opacity on scroll - nav
-var fadeStart=100 // 100px scroll or less will equiv to 1 opacity
-    ,fadeUntil=200 // 200px scroll or more will equiv to 0 opacity
-    ,fading = $('#fading')
-;
+function handleScrollOpacity() {
+  window.onscroll = function() {myFunction()};
 
-$(window).bind('scroll', function(){
-    var offset = $(document).scrollTop()
-        ,opacity=0
-    ;
-    if( offset<=fadeStart ){
-        opacity=1;
-    }else if( offset<=fadeUntil ){
-        opacity=1-offset/fadeUntil;
-    }
-    fading.css('opacity',opacity).html(opacity);
-});
+  function myFunction() {
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+          w3.addClass('#topnav', 'w3-opacity');
+      } else {           
+           w3.removeClass('#topnav','w3-opacity');
+      }
+  }
+}
+$(document).ready(handleScrollOpacity);
