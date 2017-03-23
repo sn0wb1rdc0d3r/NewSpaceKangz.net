4fdafc61f3969d2c516d89d3e01a050969bd0f40
Nate Morton <nathan.j.morton@gmail.com>
2017-03-22 22:24:05 -0700

    script for includes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+// w3.js
+function w3IncludeHTML(){
+  w3.includeHTML();
+}
+$(document).ready(w3IncludeHTML);
+
+// google map
 function initMap() {
         var HaightAshbury = {lat: 37.770015, lng: -122.446937};
         var map = new google.maps.Map(document.getElementById('GoogleMap'), {
@@ -11,12 +18,20 @@ function initMap() {
 }
 $(document).ready(initMap);
 
+// opacity on scroll - nav
+var fadeStart=100 // 100px scroll or less will equiv to 1 opacity
+    ,fadeUntil=200 // 200px scroll or more will equiv to 0 opacity
+    ,fading = $('#fading')
+;
 
-// function handleFooBar(){
-// 	var name = null;
-// 	$('#btnFoo').click(function(event) {
-//   	name = $('#txtFoo').val();
-//     $('#divBar').text('foobar ' + name)
-//   })
-//  }
-// $(document).ready(handleFooBar);
+$(window).bind('scroll', function(){
+    var offset = $(document).scrollTop()
+        ,opacity=0
+    ;
+    if( offset<=fadeStart ){
+        opacity=1;
+    }else if( offset<=fadeUntil ){
+        opacity=1-offset/fadeUntil;
+    }
+    fading.css('opacity',opacity).html(opacity);
+});
